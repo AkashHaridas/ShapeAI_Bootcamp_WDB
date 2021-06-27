b8a46f95be00d5278ef357e2f62e4c5bd123a021
AkashHaridas <86549874+AkashHaridas@users.noreply.github.com>
2021-06-27 13:38:20 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div  className = "note">
+      <h1> Shape AI Bootcamps </h1>
+      <p> A basic Web Development Bootcamp led by Shaurya Sinha. The topics covered were: HTML, CSS, Javascript and React.JS. </p>
+    </div>
+  );
+}
+export default Note;
